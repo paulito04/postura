f30e0685acc09f3d9257ea0908886ab786fe1957
paulito04 <paulitozapata04@gmail.com>
2025-12-10 12:18:50 -0500

    Fix exercise image paths

--- src/config/exerciseImages.js
+++ src/config/exerciseImages.js
@@ -7,8 +7,12 @@ export const exerciseImages = {
     require("../../assets/ejercicios/puentedegluteo1.png"),
     require("../../assets/ejercicios/puentedegluteo2.png"),
   ],
-  "apertura-pecho": [require("../../assets/ejercicios/apertura-pecho.png")],
-  "plancha-abdominal": [require("../../assets/ejercicios/plancha-abdominal.png")],
+  "apertura-pecho": [
+    require("../../assets/ejercicios/apertura de pecho.png"),
+  ],
+  "plancha-abdominal": [
+    require("../../assets/ejercicios/plancha abdominal.png"),
+  ],
   "gato-vaca": [
     require("../../assets/ejercicios/gatovaca1.png"),
     require("../../assets/ejercicios/gatovaca2.png"),
